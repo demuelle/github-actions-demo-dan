3bec247c3a38cbe207d67dce45cc96c79760fa6e
demuelle <demuelle@gmail.com>
2025-04-08 18:03:12 -0500

    Adds a comment

--- src/components/Advertisement.tsx
+++ src/components/Advertisement.tsx
@@ -4,6 +4,8 @@ interface AdvertisementProps {
   price: string;
 };
 
+// This is my advertisement component. Big Buck$$$$$$
+
 function Advertisement(props: AdvertisementProps) {
   return (
     <div className="advertisement">
